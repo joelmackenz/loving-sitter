1ebf18f0fd083c25ebcad179e93cb876fd85cea7
yliu298 <lookinglyb@hotmail.com>
2021-06-08 17:09:35 -0400

    review-model-and-controller

---
+++ server/models/Review.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    firstName: String,
+    lastName: String,
+    profileImg: String,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+  message: String,
+  postDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = Review = mongoose.model("review", reviewSchema);
