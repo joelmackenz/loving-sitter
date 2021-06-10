ca70fd7da5cf0fd3d2efdc56fdbb747a11831d6a
gurkiran_singh <gurkiransinghk@gmail.com>
2021-06-10 15:29:46 -0400

    resolved conflicts
    Signed-off-by: gurkiran_singh <gurkiransinghk@gmail.com>

--- client/src/interface/FetchOptions.ts
+++ client/src/interface/FetchOptions.ts
@@ -1,5 +1,4 @@
 export interface FetchOptions {
-<<<<<<< HEAD
 	method: string;
 	headers?: {
 		Accept?: string;
@@ -7,12 +6,4 @@ export interface FetchOptions {
 	};
 	body?: string | FormData;
 	credentials: RequestCredentials;
-=======
-  method: string;
-  headers?: {
-    'Content-Type': string;
-  };
-  body?: string;
-  credentials: RequestCredentials;
->>>>>>> 0a8b5eddc188d21f3b4f6c9a40b2850578cbba08
 }
