24e7ad940e5a570408a65099b961010962f1f85b
joelmackenz <joelmackenzie3@gmail.com>
2021-06-17 13:58:46 -0700

    Add ManageBookings styling

--- client/src/pages/ManageBookings/ManageBookings.tsx
+++ client/src/pages/ManageBookings/ManageBookings.tsx
@@ -1,26 +1,64 @@
 import { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import useStyles from './useStyles';
+import Booking from './Booking';
+import { Typography } from '@material-ui/core';
 
 export default function ManageBookings(): JSX.Element {
   const classes = useStyles();
-  const [date, changeDate] = useState<Date>(new Date());
-
-  //   const changeDate = () => {
-  //     console.log('Yes');
-  //   };
+  const [date, setDate] = useState<Date | null>(new Date());
 
   return (
-    <>
-      <Grid>
-        {/* <NextBooking />
-        <CurrentPastBookings /> */}
+    <Grid container justify="center" spacing={10} className={classes.root}>
+      <Grid item xs={10} md={6} xl={4}>
+        <Card raised={true} className={classes.bookingCard}>
+          <CardContent>
+            <Grid className={classes.cardHeader}>
+              <Typography className={classes.bookingTitle}>Your next booking:</Typography>
+            </Grid>
+            <Booking large={true} />
+          </CardContent>
+        </Card>
+        <Card raised={true} className={classes.bookingCard}>
+          <CardContent className={classes.bookingListCurrentPast}>
+            <Grid className={classes.cardHeader}>
+              <Typography className={classes.bookingTitle}>Current Bookings:</Typography>
+            </Grid>
+            <Grid>
+              <Booking outlined={true} accepted={true} />
+              <Booking outlined={true} accepted={false} />
+            </Grid>
+            <Grid className={classes.cardHeader}>
+              <Typography className={classes.bookingTitle}>Past Bookings:</Typography>
+            </Grid>
+            <Grid>
+              <Booking outlined={true} accepted={true} />
+              <Booking outlined={true} accepted={true} />
+            </Grid>
+          </CardContent>
+        </Card>
       </Grid>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker autoOk orientation="landscape" variant="static" openTo="date" value={date} onChange={changeDate} />
-      </MuiPickersUtilsProvider>
-    </>
+      <Grid item>
+        <Card raised={true}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <DatePicker
+              fullWidth
+              showTodayButton={false}
+              autoOk
+              orientation="landscape"
+              variant="static"
+              openTo="date"
+              value={date}
+              onChange={(newDate) => setDate(newDate)}
+              disableToolbar={true}
+            />
+          </MuiPickersUtilsProvider>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
--- client/src/pages/ManageBookings/useStyles.ts
+++ client/src/pages/ManageBookings/useStyles.ts
@@ -1,8 +1,64 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-  datePicker: {
+  root: {
+    flexGrow: 1,
+    marginTop: '5rem',
+  },
+  bookingCard: {
+    marginBottom: '1rem',
+  },
+  booking: {
+    boxShadow: 'none',
+  },
+  cardHeader: {
+    padding: '1rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bookingHeader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  bookingTitle: {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  bookingAvatar: {
+    marginRight: '1rem',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+  },
+  bookingProfileGrid: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bookingUserInfoGrid: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  bookingAcceptedDeclined: {
+    textTransform: 'uppercase',
+    color: 'grey',
+    alignSelf: 'center',
+    marginRight: '2rem',
+  },
+  settingsIcon: {
+    color: 'lightgrey',
+  },
+  bookingList: {
     display: 'block',
+  },
+  bookingListCurrentPast: {
+    height: '50vh',
+    overflowY: 'scroll',
+    overflowX: 'hidden',
+  },
+  currentPastBooking: {
+    border: '1px solid lightgrey',
   },
 });
 
