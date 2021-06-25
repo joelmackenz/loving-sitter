e40962e6a612a489fd35ad567c54ddb8cba2b190
gurkiran_singh <gurkiransinghk@gmail.com>
2021-06-25 00:38:38 -0400

    added search state in main
    Signed-off-by: gurkiran_singh <gurkiransinghk@gmail.com>

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -55,7 +55,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
           setLoggedInUser(null);
           history.push({
-            pathname: '/login',
+            pathname: '/',
             state: { previousPath: location.pathname },
           });
         }
